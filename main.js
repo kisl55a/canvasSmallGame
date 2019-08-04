b5cfca4095863c93c6a0bf0cf898884e74dc7f65
Dmitrii <kislenko1910@mail.ru>
2019-08-04 19:40:46 +0500

    bullets have been added

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var x1 = -30 ;
+var startedAngle = -30 ;
+gravity = 0.5
+friction = 0.5;
+var bulletsArray = [];
 var c = canvas.getContext('2d');
 var targetsArray = [];
  function sin(a) {
@@ -23,6 +26,47 @@ window.addEventListener('mousemove', function (event) {
     mouse.y = event.y;
 });
 
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'ArrowDown') {
+        startedAngle = startedAngle + 4;
+       
+    }
+    if (event.code == 'ArrowUp') {
+        startedAngle = startedAngle - 4;
+    }
+    if ( event.code == 'Space') {
+        gun.shoot();
+        console.log('shoot');
+    }
+  }); 
+function Bullet(dx, dy) {
+    this.dx = dx;
+    this.dy = dy;
+    this.x = gun.x;
+    this.y = gun.y;
+    this.rad = 10;
+    this.color = 'red';
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.rad, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    this.update = function() {
+        if ( this.x + this.rad > innerWidth || this.x - this.rad < 0 ) {
+            this.dx = -this.dx;
+        } 
+        if ( this.y + this.rad > innerHeight || this.y - this.rad < 0 ) {
+            this.dy = -this.dy * friction;
+        } else {
+            this.dy += gravity;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+}
 
 function Target(x, y, dx, dy, rad, color) {
     this.x = x;
@@ -43,25 +87,16 @@ function Target(x, y, dx, dy, rad, color) {
             this.dx = -this.dx;
         } 
         if ( this.y + this.rad > innerHeight || this.y - this.rad < 0 ) {
-            this.dy = -this.dy;
+            this.dy = -this.dy * friction;
+        } else {
+            this.dy += gravity;
         }
-
         this.x += this.dx;
         this.y += this.dy;
-        // console.log( Math.sin(mouse.x))  
     }
 }
 
-document.addEventListener('keydown', function(event) {
-    if (event.code == 'ArrowDown') {
-        x1 = x1 + 4;
-       
-    }
-    if (event.code == 'ArrowUp') {
-        x1 = x1 - 4;
-       
-    }
-  }); 
+
 
 function Gun (x,y) {
     this.x = x;
@@ -78,15 +113,17 @@ function Gun (x,y) {
     this.update = function() {
         c.save(); 
         c.translate(40, 600);
-        c.rotate(inRad(x1));
+        c.rotate(inRad(startedAngle));
         // console.log(mouse.x, mouse.y);
         c.fillStyle= 'black';
         c.fillRect( 0, 0, this.width, this.height);
         c.restore(); 
-        
+    }
+    this.shoot = function () { 
+        bulletsArray.push( new Bullet(10, 10));
     }
 }
-var gun = new Gun(10, 10)
+var gun = new Gun(10, 10);
 function init() {
     for (let i = 0; i < 5; i++) {
         let x = getRandomInt(30, innerWidth - 30);
@@ -112,6 +149,10 @@ function animate() {
     for (let index = 0; index < targetsArray.length; index++) {
         targetsArray[index].update();
         targetsArray[index].draw();
+    }
+    for (let index = 0; index < bulletsArray.length; index++) {
+        bulletsArray[index].update();
+        bulletsArray[index].draw();
     }
     gun.draw();
     gun.update();
